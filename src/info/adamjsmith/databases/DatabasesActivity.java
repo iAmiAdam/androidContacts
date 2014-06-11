e02bbd92924d30868097da591305c86e00998895
Adam Smith <adam.smith18@ntlworld.com>
2014-06-11 10:54:05 +0100

    Creating a DB in design time

--- src/info/adamjsmith/databases/DatabasesActivity.java
+++ src/info/adamjsmith/databases/DatabasesActivity.java
@@ -1,10 +1,16 @@
 package info.adamjsmith.databases;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.Toast;
-
 
 public class DatabasesActivity extends Activity {
 
@@ -45,6 +51,29 @@ public class DatabasesActivity extends Activity {
     	Toast.makeText(this,  "Update failed.", Toast.LENGTH_LONG).show();
     db.close();*/
     
+        try {
+        	String destPath = "/data/data/" + getPackageName() + "/databases";
+        	File f = new File(destPath);
+        	if (!f.exists()) {
+        		f.mkdirs();
+        		f.createNewFile();
+        		
+        		CopyDB(getBaseContext().getAssets().open("mydb"), new FileOutputStream(destPath + "MyDB"));
+        	}
+        } catch (FileNotFoundException e) {
+        	e.printStackTrace();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+        
+        db.open();
+        Cursor c = db.getAllContacts();
+        if(c.moveToFirst()) {
+        	do {
+        		DisplayContact(c);
+        	} while (c.moveToNext());
+        }
+        db.close();
     }
     
     public void DisplayContact(Cursor c) {
@@ -52,4 +81,14 @@ public class DatabasesActivity extends Activity {
     					"name: " + c.getString(1) + "\n" +
     					"Email: " + c.getString(2), Toast.LENGTH_LONG).show();
     } 
+    
+    public void CopyDB(InputStream inputStream, OutputStream outputStream) throws IOException {
+    	byte[] buffer = new byte[1024];
+    	int length;
+    	while ((length = inputStream.read(buffer)) > 0) {
+    		outputStream.write(buffer, 0, length);
+    	}
+    	inputStream.close();
+    	outputStream.close();
+    }
 }
